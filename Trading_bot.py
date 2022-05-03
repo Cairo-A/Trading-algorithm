8a1edf6d8cfd72fac08eba2206d04eac708f20c1
Cairo-A <73303888+Cairo-A@users.noreply.github.com>
2022-05-02 23:16:32 -0400

    Update Trading_bot.py

--- Trading_bot.py
+++ Trading_bot.py
@@ -2,7 +2,7 @@
 #CSCI-135 Group Project Final
 #Date: 5-02-22
 #Trading bot
-
+#This is the current version implemented as of 5/2/22
 def stock_market(capital,stock_price):
   max_prof_val, curr_max_val = 0, 0
   for price in reversed(stock_price):
@@ -14,7 +14,7 @@ def stock_market(capital,stock_price):
     max_prof_val = max(pot_profit, max_prof_val)
   
     min_p = min(stock_price)
-#no . of stocks
+#number of stocks
     n_stock = capital / min_p
   
   return n_stock*max_prof_val
@@ -23,12 +23,10 @@ def stock_market(capital,stock_price):
 Name = input("Please enter customer's name: ")
 Email = input("Please enter customer's email: ")
 Phone = input("Please enter customer's phone number: ")
-capital = int(input("How much would the customer like to start with?: "))
+capital = int(input("How much money would the customer like to invest with?: "))
 #potential profit
-#This example utilizes Alphabet Inc. last 10 days' closing prices. (4/18/22-4/29/22)
-print('Name:' ,Name, 'Email:' ,Email, 'Phone #:' ,Phone, 'Profit:' ,stock_market(capital,[2299.33, 2388.23, 2300.41, 2390.12, 2465.00, 2392.28, 2498.75, 2564.91, 2610.62, 2559.22]))
+#This example utilizes the regular prices of all stocks pulled from Webscraper except Nasdaq. (5/02/22)
+# For future updates, I would implement the ability to let the user change the numbers in the list. For now, the link in the final documentation should give you the ability to edit that part if you want to change the numbers
+print('Name:' ,Name, 'Email:' ,Email, 'Phone #:' ,Phone, 'Profit:' , stock_market(capital, [2343.14, 2490, 48.71, 381.23, 178.64, 19.06, 10.48, 902.94, 49.14, 46.23, 211.53, 40.96]))
 #final balance
-print('Final Balance:', capital+stock_market(capital,[2299.33, 2388.23, 2300.41, 2390.12, 2465.00, 2392.28, 2498.75, 2564.91, 2610.62, 2559.22]))
-
-
-#Citations will be added to the final documents
+print('Final Balance:', capital+stock_market(capital,[2343.14, 2490, 48.71, 381.23, 178.64, 19.06, 10.48, 902.94, 49.14, 46.23, 211.53, 40.96]))
